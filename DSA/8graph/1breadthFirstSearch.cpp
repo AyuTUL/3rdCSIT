eb71dc06d867496eed628710cf2011933c9c7cb0
AyuTUL <ayushtuladhar007@gmail.com>
2025-04-19 22:34:08 +0545

    avlbaki

---
+++ DSA/8graph/1breadthFirstSearch.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+#define MAX 100
+using namespace std;
+class Queue 
+{
+    private:
+        int a[MAX],rear,front;
+    public:
+        Queue()
+        {
+            rear=front=-1;
+        }
+        bool isEmpty()
+        {
+            return(front==-1 && rear==-1);
+        }
+        void enqueue(int item)
+        {
+            if(rear==MAX-1)
+                cout<<endl<<"Queue overflow"<<endl;
+            else
+            {
+                if(isEmpty())
+                    rear=front=0;
+                else
+                    rear++;
+                a[rear]=item;
+            }
+        }
+        int dequeue()
+        {
+            int item;
+            if(isEmpty())
+            {
+                cout<<endl<<"Queue underflow"<<endl;
+                return -1;
+            }
+            else
+            {
+                item=a[front];
+                if(rear==front)
+                    rear=front=-1;
+                else
+                    front++;
+                return item;    
+            }           
+        }
+};
+
+void BFS(int g[MAX][MAX],int src,int V,char v[]) 
+{
+    bool visited[MAX]={false};
+    Queue q;
+    visited[src]=true;
+    q.enqueue(src);
+    cout<<endl<<"BFS traversal : ";
+    while(!q.isEmpty()) 
+    {
+        int u=q.dequeue();
+        cout<<v[u]<<" ";
+        for(int i=0;i<V;i++) 
+            if(g[u][i]==1 && !visited[i]) 
+            {
+                visited[i]=true;
+                q.enqueue(i);
+            }
+    }
+}
+int main() 
+{
+    int V,g[MAX][MAX]={0},srcIndex=-1;
+    char v[MAX],source; 
+    cout<<"Enter no. of vertices : ";
+    cin>>V;
+    cout<<"Enter "<<V<<" vertices : ";
+    for(int i=0;i<V;i++)
+        cin>>v[i];
+    cout<<endl<<"Enter adjacency matrix :"<<endl;
+    cout<<"  ";
+    for(int i =0;i<V;i++)
+        cout<<" "<<v[i];
+    cout<<endl;
+	for(int i=0;i<V;i++)
+    {
+    	cout<<" "<<v[i]<<" ";
+    	for(int j=0;j<V;j++)
+		   cin>>g[i][j]; 
+	}
+    cout<<endl<<"Enter source vertex : ";
+    cin>>source;
+    for(int i=0;i<V;i++) 
+        if(v[i]==source) 
+        {
+            srcIndex=i;
+            break;
+        }
+    if(srcIndex==-1) 
+    {
+        cout<<"Invalid source vertex"<<endl;
+        return 1;
+    }
+    BFS(g,srcIndex,V,v);
+    return 0;
+}
---
+++ DSA/8graph/3dijkstraAlgorithm.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<iomanip>
+#define MAX 100  
+#define INF 999999
+using namespace std;
+int minDistance(int d[],bool visited[],int V) 
+{
+    int min=INF,min_index=-1;
+    for(int v=0;v<V;v++)
+        if(!visited[v] && d[v]<=min) 
+        {
+        	min=d[v];
+			min_index=v;
+		}      
+    return(min_index);
+}
+char source;
+void print(int d[], int V, char v[]) 
+{
+    cout<<endl<<"+----------+-----------------------------+"<<endl;
+    cout<<"|  Vertex  |  Distance from Source ( "<<source<<" ) |";
+    cout<<endl<<"+----------+-----------------------------+"<<endl;
+    for(int i = 0; i < V; i++) 
+        cout<<"|"<<setw(5)<<v[i]<<setw(6)<<"|"<<setw(15)<<d[i]<<setw(15)<<"|"<<endl;
+    cout<<"+----------+-----------------------------+"<<endl;
+}
+void dijkstra(int g[MAX][MAX],int src,int V,char v[]) 
+{
+    int d[MAX]; 
+    bool visited[MAX];
+    for(int i=0;i<V;i++)
+    {
+    	d[i]=INF;
+		visited[i]=false;
+	} 
+    d[src]=0;
+    for(int i=0;i<V-1;i++) 
+	{
+        int u=minDistance(d,visited,V);
+        visited[u]=true;
+        for(int v=0;v<V;v++)
+            if(!visited[v] && g[u][v] && d[u]!=INF && d[u]+g[u][v]<d[v]) 
+                d[v]=d[u]+g[u][v];
+    }
+    print(d,V,v);
+}
+int main() 
+{
+    int V,g[MAX][MAX]={0},srcIndex=-1;
+    char v[MAX]; 
+    cout<<"Enter no. of vertices : ";
+    cin>>V;
+    cout<<"Enter "<<V<<" vertices : ";
+    for(int i=0;i<V;i++)
+        cin>>v[i];
+    cout<<endl<<"Enter adjacency matrix :"<<endl;
+    cout<<"  ";
+    for(int i =0;i<V;i++)
+        cout<<" "<<v[i];
+    cout<<endl;
+	for(int i=0;i<V;i++)
+    {
+    	cout<<" "<<v[i]<<" ";
+    	for(int j=0;j<V;j++)
+		   cin>>g[i][j]; 
+	}
+    cout<<endl<<"Enter source vertex : ";
+    cin>>source;
+    for(int i=0;i<V;i++) 
+        if(v[i]==source) 
+        {
+            srcIndex=i;
+            break;
+        }
+    if(srcIndex==-1) 
+    {
+        cout<<"Invalid source vertex"<<endl;
+        return 1;
+    }
+    dijkstra(g,srcIndex,V,v);
+    return 0;
+}

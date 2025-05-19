d6bd8fa1973d0375077004c206f82fe994fa6d2d
AyuTUL <ayushtuladhar007@gmail.com>
2025-05-19 21:56:31 +0545

    tth

--- CA/Lab/11controlUnit.vhd
+++ CA/Lab/11controlUnit.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 entity control_unit is
     Port (
         opcode     : in  STD_LOGIC_VECTOR(1 downto 0);
@@ -10,12 +11,12 @@ architecture Behavioral of control_unit is
 begin
     process(opcode)
     begin
-        case opcode is
-            when "00" => ctrl_signal <= "00";  -- No operation
-            when "01" => ctrl_signal <= "01";  -- Load
-            when "10" => ctrl_signal <= "10";  -- Store
-            when "11" => ctrl_signal <= "11";  -- Add
-            when others => ctrl_signal <= "00";
+        case to_integer(unsigned(opcode)) is
+            when 0 => ctrl_signal <= "00";  -- No operation
+            when 1 => ctrl_signal <= "01";  -- Load
+            when 2 => ctrl_signal <= "10";  -- Store
+            when 3 => ctrl_signal <= "11";  -- Add
+            when others => ctrl_signal <= "00"; -- Default case
         end case;
     end process;
 end Behavioral;
